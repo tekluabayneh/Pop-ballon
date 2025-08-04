75c561eebcd75e31cb2d7e67cb9294698bcd047f
tekluabayneh <tekluabayneh@gmail.com>
2025-08-04 09:54:56 +0000

    change relative asset path

--- src/Game/scenes/PreloadScene.ts
+++ src/Game/scenes/PreloadScene.ts
@@ -1,19 +1,19 @@
+import bgImg from "./../../../public/assets/bg.png"
+import arrowImg from "../../../public/assets/arrow2.png"
+import ballonImg from "../../../public/assets/ballon2.png"
 
-
- export class PreloadScene extends Phaser.Scene {
+export class PreloadScene extends Phaser.Scene {
     constructor() {
         super("PreloadScene")
     }
 
-        preload(){
-            this.load.image("bg", "/assets/bg.png")
-            this.load.image("arrow", "/assets/arrow2.png")
-            this.load.image("ballon", "/assets/ballon2.png")
-        }
+    preload() {
+        this.load.image("bg", bgImg)
+        this.load.image("arrow", arrowImg)
+        this.load.image("ballon", ballonImg)
+    }
 
-        create(): void{
-            this.scene.start("MainScene")
+    create(): void {
+        this.scene.start("MainScene")
     }
 }
-
-
